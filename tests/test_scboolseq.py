045e9a9a2780d242b61a690c6f135ed1ac084401
Samuel Pastva <sam.pastva@gmail.com>
2024-12-31 21:18:20 +0100

    Simple discarded gene test.

--- tests/test_scboolseq.py
+++ tests/test_scboolseq.py
@@ -5,9 +5,17 @@ import pandas as pd
 def test_version():
     assert __version__ == "0.2.0"
 
-def test_single_gene_data():    
+def test_single_bimodal_gene_data():    
     pd_data = pd.read_csv('./tests/input-data.csv', index_col=0)
     scboolseq = scb.scBoolSeq()
     scboolseq.fit(pd_data)
     pd_data_bin = scboolseq.binarize(pd_data)    
     assert set(pd_data_bin['Tcea1']) == {0.0, 1.0}    
+
+def test_single_discarded_gene_data():    
+    pd_data = pd.read_csv('./tests/input-data-2.csv', index_col=0)
+    scboolseq = scb.scBoolSeq()
+    scboolseq.fit(pd_data)
+    pd_data_bin = scboolseq.binarize(pd_data)    
+    print(scboolseq.criteria_)
+    assert set(pd_data_bin['Tcea1']) == {}
