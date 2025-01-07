311a92550921008d0393bcea496c6397c14d1afe
Samuel Pastva <sam.pastva@gmail.com>
2025-01-07 13:30:25 +0100

    Test bandwidth computation and return NaN if it fails.

--- tests/test_scboolseq.py
+++ tests/test_scboolseq.py
@@ -21,3 +21,14 @@ def test_single_discarded_gene_data():
     assert scboolseq.criteria_.iloc[0]['Category'] == 'Discarded'
     assert 0.0 not in pd_data_bin['Tcea1']
     assert 1.0 not in pd_data_bin['Tcea1']
+
+def test_problematic_KDE_bandwidth():    
+    pd_data = pd.read_csv('./tests/input-data-3.csv', index_col=0)
+    scboolseq = scb.scBoolSeq()
+    scboolseq.fit(pd_data)
+    pd_data_bin = scboolseq.binarize(pd_data)
+    # This gets recognized as zero inflated, but otherwise is 
+    # completely binarized as NaN.
+    assert scboolseq.criteria_.iloc[0]['Category'] == 'ZeroInf'
+    assert 0.0 not in pd_data_bin['Gm7293']
+    assert 1.0 not in pd_data_bin['Gm7293']
